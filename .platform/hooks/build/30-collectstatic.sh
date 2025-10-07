1217f8ea4bae33c958eaad27bab4d535538da9ee
Seongjae Jung <sj4771@nyu.edu>
2025-10-07 18:06:08 -0400

    Move EB build steps to predeploy hooks

--- .platform/hooks/build/30-collectstatic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-APP_DIR="/var/app/staging"
-cd "$APP_DIR/backend"
-
-python manage.py collectstatic --noinput
--- .platform/hooks/predeploy/30-collectstatic.sh
+++ .platform/hooks/predeploy/30-collectstatic.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 APP_DIR="/var/app/staging"
 cd "$APP_DIR/backend"
 
-/var/app/venv/*/bin/python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
