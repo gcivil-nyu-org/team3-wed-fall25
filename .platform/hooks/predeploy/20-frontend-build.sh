5eadd87fda09984470af1b940cbea22e381b3e97
Seongjae Jung <sj4771@nyu.edu>
2025-10-07 18:06:08 -0400

    Move EB build steps to predeploy hooks

--- .platform/hooks/predeploy/20-frontend-build.sh
+++ .platform/hooks/predeploy/20-frontend-build.sh
@@ -5,7 +5,7 @@ APP_DIR="/var/app/staging"
 cd "$APP_DIR/frontend"
 
 npm ci
-npm run build  # tsc -b && vite build && node copy-index.cjs
+npm run build  # tsc -b && vite predeploy && node copy-index.cjs
 
 ls -la "$APP_DIR/backend/static/_app" || true
 ls -la "$APP_DIR/backend/templates" || true
